08b94859b9b064b9f8951a45ce9789bffb4712e6
yuuuuki-k <kawauchiyuuuuki@gmail.com>
2020-10-13 16:05:21 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    get_week
     @plan = Plan.new
   end
 
@@ -18,14 +18,21 @@ class CalendarsController < ApplicationController
     params.permit(:date, :plan)
   end
 
-  def getWeek
+  def get_week
+   
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
+    #★これは添字。各曜日をwdaysに入れている。
+    
+    # wdays[1]→(月)
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
+    #★Date.todayを@todays_date　のインスタンス変数にしている
+   
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
+    #★@week_daysの値が添字となっていく
+    
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
@@ -34,9 +41,19 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      
+      wday_num = Date.today.wday + x #Date.today.wdayを利用して添字となる数値を得る
+
+      #もしもwday_numが7以上であれば、7を引く
+      if wday_num >= 7
+        wday_num = wday_num - 7
+      end
+      days = { month: (@todays_date+x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[(@todays_date+x).wday]}
       @week_days.push(days)
+      
     end
 
   end
 end
+
+
